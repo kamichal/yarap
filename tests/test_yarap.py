a1cca00e3ded05f4cd9e1aec55b56d537e63ad21
kamichal <bucowski@gmail.com>
2017-09-24 13:26:17 +0200

    added navigation

--- tests/test_yarap.py
+++ tests/test_yarap.py
@@ -6,7 +6,7 @@ Created on 24 Sep 2017
 '''
 import os
 import pytest
-from yarap.yawrap import Yawrap
+from yarap.yawrap import Yawrap, NavedYawrap
 
 
 @pytest.fixture(scope='session')
@@ -19,8 +19,9 @@ def out_dir():
 
 
 def test_1(out_dir):
-    assert str(Yawrap()) == ''
-    render = Yawrap().render()
+    jarap = Yawrap('')
+    assert str(jarap) == ''
+    render = jarap._render_page()
     assert render == """\
 <!doctype html>
 <html lang="en-US">
@@ -34,7 +35,7 @@ def test_1(out_dir):
 def test_2(out_dir):
     the_file = os.path.join(out_dir, 'test_2.html')
 
-    jawrap = Yawrap('ol rajt!')
+    jawrap = Yawrap(the_file, 'ol rajt!')
     jawrap.css("body {padding: 12px;}")
 
     with jawrap.tag('div'):
@@ -46,8 +47,8 @@ def test_2(out_dir):
   <p>Nothing much here.</p>
 </div>"""
 
-    render = jawrap.render()
-    jawrap.render_to_file(the_file)
+    render = jawrap._render_page()
+    jawrap.render()
     assert render == """\
 <!doctype html>
 <html lang="en-US">
@@ -63,3 +64,44 @@ def test_2(out_dir):
   </body>
 </html>"""
     assert os.path.isfile(the_file)
+
+
+
+def test_navigation(out_dir):
+
+    def make_content(jarap_, text):
+        with jarap_.tag('p'):
+            jarap_.text(text)
+        with jarap_.tag('p'):
+            jarap_.text("And I'm enjoying the navigation.")
+
+    index_file = os.path.join(out_dir, 'test_subs_index.html')
+    parent = NavedYawrap(index_file, 'Index')
+    make_content(parent, "I'm a parent")
+
+    child_file0 = os.path.join(out_dir, 'test_subs_child0.html')
+    child0 = parent.sub(child_file0, 'child 0')
+    make_content(child0, "I'm a child 0")
+
+    child_file1 = os.path.join(out_dir, 'test_subs_child1.html')
+    child1 = parent.sub(child_file1, 'child 1')
+    make_content(child1, "I'm a child 1")
+
+    child_file2 = os.path.join(out_dir, 'test_subs_subdir', 'test_subs_child2.html')
+    child2 = child1.sub(child_file2)
+    make_content(child2, "I'm a child 2. No title")
+
+    child_file3 = os.path.join(out_dir, 'test_subs_subdir', 'test_subs_child3.html')
+    child3 = parent.sub(child_file3)
+    make_content(child3, "I'm a child 3. I don't have a title.")
+
+    parent.render_all_files()
+
+    assert all(os.path.isfile(f) for f in (index_file, 
+                                           child_file1,
+                                           child_file2,
+                                           child_file3))
+
+    print child2._get_nav_structure()
+
+    
--- yarap/yawrap.py
+++ yarap/yawrap.py
@@ -6,29 +6,22 @@ Created on 23 Sep 2017
 '''
 
 from contextlib import contextmanager
-import os
 import yattag
+import os
+import weakref
 
 
 class Yawrap(object):
     html_d = dict(lang="en-US")
     meta_d = [dict(charset="UTF-8")]
 
-    def __init__(self, title=''):
-        self.doc, self.tag, self.text, self.line = yattag.SimpleDoc().ttl()
+    def __init__(self, target_file, title='', parent=None):
+        self.target_file = target_file
         self.title = title
+        self._parent = parent
+        self.doc, self.tag, self.text, self.line = yattag.SimpleDoc().ttl()
         self._js = []
         self._css = []
-        self.body = ''
-
-    @contextmanager
-    def sub(self):
-        sub_ = Yawrap()
-        yield sub_
-        self.doc.asis(yattag.indent(sub_.doc.getvalue()))
-        self._js.extend(sub_._js)
-        self._css.extend(sub_._css)
-        print self
 
     def css(self, *css_defs):
         self._css.extend(css_defs)
@@ -37,30 +30,90 @@ class Yawrap(object):
         body_render = yattag.indent(self.doc.getvalue())
         return body_render
 
-    def render(self):
-        doc, tag, text = yattag.SimpleDoc().tagtext()
-        doc.asis('<!doctype html>')
-        with tag('html', **self.html_d):
-            with tag('head'):
+    @contextmanager
+    def _page_structure(self, page_doc):
+        page_doc.asis('<!doctype html>')
+        with page_doc.tag('html', **self.html_d):
+            with page_doc.tag('head'):
                 for meta in self.meta_d:
-                    doc.stag('meta', **meta)
+                    page_doc.stag('meta', **meta)
 
                 if self.title:
-                    with tag('title'):
-                        text(self.title)
+                    with page_doc.tag('title'):
+                        page_doc.text(self.title)
                 for js in self._js:
-                    with tag('script'):
-                        text(js)
+                    with page_doc.tag('script'):
+                        page_doc.text(js)
 
                 for css in self._css:
-                    with tag('style'):
-                        text(css)
+                    with page_doc.tag('style'):
+                        page_doc.text(css)
+            with page_doc.tag('body'):
+                yield
 
-            with tag('body'):
-                doc.asis(str(self))
+    def _render_page(self):
+        page_doc = yattag.SimpleDoc()
+        with self._page_structure(page_doc):
+            page_doc.asis(str(self))
+        return yattag.indent(page_doc.getvalue())
 
-        return yattag.indent(doc.getvalue())
+    def render(self):
+        dir_ = os.path.dirname(self.target_file)
+        if not os.path.isdir(dir_):
+            assert not os.path.isfile(self.target_file)
+            os.makedirs(dir_)
 
-    def render_to_file(self, target_file):
-        with open(target_file, 'wt') as ff:
-            ff.write(self.render())
+        with open(self.target_file, 'wt') as ff:
+            ff.write(self._render_page())
+
+
+class NavedYawrap(Yawrap):
+
+    def __init__(self, target_file, title='', parent=None, nav_title=''):
+        super(NavedYawrap, self).__init__(target_file, title, parent)
+        self.nav_title = nav_title or title
+        self._separate_subs = []
+
+    def sub(self, target_file, title=''):
+        sub_ = NavedYawrap(target_file, title, weakref.ref(self))
+        self._separate_subs.append(sub_)
+        return sub_
+
+    def _render_page(self):
+        page_doc = yattag.SimpleDoc()
+        with self._page_structure(page_doc):
+            if self._separate_subs or self._parent:
+                self._insert_nav(page_doc)
+            page_doc.asis(str(self))
+        return yattag.indent(page_doc.getvalue())
+
+    def render_all_files(self):
+        self.render()
+        for sub in self._separate_subs:
+            sub.render_all_files()
+
+    def _get_root(self):
+        if self._parent:
+            return self._parent()._get_root()
+        return self
+
+    def _get_nav_structure(self, sub=None):
+        root = sub or self._get_root()
+        its_children = [child._get_nav_structure(child) for child in root._separate_subs]
+        return (root, its_children)
+
+    def _insert_nav(self, doc):
+        nav_structure = self._get_nav_structure()
+        curr_dir = os.path.dirname(self.target_file)
+
+        def render_subs_of(structure_element):
+            current, subs = structure_element
+            with doc.tag('div', klass='nav_group_div'):
+                link = os.path.relpath(current.target_file, curr_dir)
+                with doc.tag('a', href=link):
+                    doc.text(current.nav_title or link.replace('.html', ''))
+                for sub in subs:
+                    render_subs_of(sub)
+
+        with doc.tag('div', klass='nav_main_panel'), doc.tag('nav'):
+            render_subs_of(nav_structure)
