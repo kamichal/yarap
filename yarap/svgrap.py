a3d401f60f1efbedcb73404ca378a164827bafc9
kamichal <bucowski@gmail.com>
2017-09-26 01:17:07 +0200

    changed idea for svg

--- yarap/svgrap.py
+++ yarap/svgrap.py
@@ -57,61 +57,6 @@ yattag.simpledoc._attributes = _attributes2
 DEFAULT_SVG_TAG_ATTRIBUTES = dict(xmlns="http://www.w3.org/2000/svg", version="1.1")
 
 
-class Svgrap(yattag.SimpleDoc):
-    svg_d = DEFAULT_SVG_TAG_ATTRIBUTES
-    svg_style_def = ''
-
-    def __init__(self, stag_end=' />', *args, **kwargs):
-        super(Svgrap, self).__init__(stag_end)
-        self.args = args
-        self.kwargs = kwargs
-
-    def save_to_file(self, target_file):
-        dir_ = os.path.dirname(target_file)
-        if not os.path.isdir(dir_):
-            assert not os.path.isfile(dir_)
-            os.makedirs(dir_)
-
-        with open(target_file, 'wt') as ff:
-            ff.write('<?xml version="1.0" encoding="UTF-8" ?>\n')
-            ff.write(self._render_svg())
-
-    def render_impl(self, painter):
-        pass
-
-    def _render_svg(self, parent_doc=None):
-        target_doc = parent_doc or yattag.SimpleDoc()
-        if not isinstance(target_doc, yattag.SimpleDoc):
-            raise ValueError('Expected yattag.SimpleDoc instance.')
-
-        with self._svg_structure(target_doc, *self.args, **self.kwargs):
-            target_doc.asis(self.getvalue())
-
-        if parent_doc:
-            " make it happen "
-            pass
-        else:
-            return yattag.indent(target_doc.getvalue())
-
-    @contextmanager
-    def _svg_structure(self, parent_doc, *args, **kwargs):
-        kwargs.update(self.svg_d)
-        with parent_doc.tag('svg', *args, **kwargs):
-            if self.svg_style_def:
-                with parent_doc.tag('style', type="text/css"):
-                    parent_doc.cdata(self.svg_style_def)
-            yield
-
-
-def assert_it_is_yattag(obj, yattag_type=yattag.SimpleDoc):
-    if not isinstance(obj, yattag_type):
-        raise ValueError('Expected yattag.SimpleDoc instance.')
-
-
-def assert_it_is_yattag_or_none(obj, yattag_type=yattag.SimpleDoc):
-    assert_it_is_yattag(obj, (yattag_type, None))
-
-
 @contextmanager
 def svg_structure(painter,
                   svg_tag_attributes=DEFAULT_SVG_TAG_ATTRIBUTES,
@@ -131,33 +76,14 @@ def svg_structure(painter,
         yield
 
 
-class SvgClassBase(object):
-    svg_standalone_xml_header = '<?xml version="1.0" encoding="UTF-8" ?>\n'
+class SvgHost(yattag.SimpleDoc):
     svg_d = DEFAULT_SVG_TAG_ATTRIBUTES
-    svg_style_def = ''
 
-    def __init__(self, parent_doc):
-        assert_it_is_yattag(parent_doc)
-        self._parent_doc = parent_doc
+    def __init__(self, *args, **kwargs):
+        super(SvgHost, self).__init__(*args, **kwargs)
 
-    def save_to_file(self, target_file, *args, **kwargs):
-        empty_doc = yattag.SimpleDoc()
-        svg_code = self._create_svg_code(empty_doc, *args, **kwargs)
-        with open(make_place(target_file), 'wt') as ff:
-            ff.write(self.__class__.svg_standalone_xml_header)
-            ff.write(svg_code)
-
-    def _create_svg_code(self, *args, **kwargs):
-        with svg_structure(self._parent_doc, svg_tag_attributes=self.__class__.svg_d):
-            self.render_impl(self._parent_doc)
-        return yattag.indent(self._parent_doc.getvalue())
-
-    def render_impl(self, painter, *args, **kwargs):
-        " embed implement it in a derived class "
-        pass
-
-    @classmethod
-    def embed_svg(cls, target_doc):
-        """ not tested yet """
-        assert_it_is_yattag(target_doc)
-        cls._create_svg_code(target_doc)
+    @contextmanager
+    def svg(self, svg_styles_as_str='', *args, **svg_tag_attributes):
+        svg_tag_attributes.update(self.svg_d)
+        with svg_structure(self, svg_tag_attributes, svg_styles_as_str, *args, **svg_tag_attributes):
+            yield
--- yarap/tests/test_svgrap.py
+++ yarap/tests/test_svgrap.py
@@ -5,13 +5,13 @@ Created on 25 wrz 2017
 
 @author: kamichal
 '''
-from yarap.svgrap import Svgrap, SvgClassBase
+from yarap.svgrap import  SvgHost
 from bs4 import BeautifulSoup
 import yattag
 import pytest
 import os
 
-EXPECTED_TEST1_INLINE_OUTPUT = """\
+EXPECTED_TEST1_OUTPUT = """\
 <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
   <rect x="25" y="25" width="200" height="200" fill="lime" stroke-width="4" stroke="pink" />
   <circle cx="125" cy="125" r="75" fill="orange" />
@@ -20,7 +20,13 @@ EXPECTED_TEST1_INLINE_OUTPUT = """\
 </svg>"""
 
 
-EXPECTED_TEST1_STANDALONE_OUTPUT = '<?xml version="1.0" encoding="UTF-8" ?>\n' + EXPECTED_TEST1_INLINE_OUTPUT
+def painter_def_test1(smth):
+    smth.stag('rect', x="25", y="25", width="200", height="200", fill="lime", stroke_width="4", stroke="pink")
+    smth.stag('circle', cx="125", cy="125", r="75", fill="orange")
+    smth.stag('polyline', ('stroke-width', 4),
+              points="50,150 50,200 200,200 200,100", stroke="red", fill="none")
+    smth.stag('line', x1="50", y1="50", x2="200", y2="200", stroke="blue", stroke_width="4")
+
 
 
 def compare_htmls(render, expected):
@@ -29,116 +35,18 @@ def compare_htmls(render, expected):
     assert gotten_soup == expected_soup
 
 
-def test_1_inline_version():
+def test_svg_as_a_tag_empty():
 
-    svg = Svgrap()
-
-    svg.stag('rect', x="25", y="25", width="200", height="200", fill="lime", stroke_width="4", stroke="pink")
-    svg.stag('circle', cx="125", cy="125", r="75", fill="orange")
-    svg.stag('polyline', ('stroke-width', 4), points="50,150 50,200 200,200 200,100", stroke="red", fill="none")
-    svg.stag('line', x1="50", y1="50", x2="200", y2="200", stroke="blue", stroke_width="4")
-
-    compare_htmls(svg._render_svg(), EXPECTED_TEST1_INLINE_OUTPUT)
-
-
-def test_1_child_version():
-
-    empty_doc = yattag.SimpleDoc()
-
-    svg = Svgrap()
-
-    svg.stag('rect', x="25", y="25", width="200", height="200", fill="lime", stroke_width="4", stroke="pink")
-    svg.stag('circle', cx="125", cy="125", r="75", fill="orange")
-    svg.stag('polyline', ('stroke-width', 4), points="50,150 50,200 200,200 200,100", stroke="red", fill="none")
-    svg.stag('line', x1="50", y1="50", x2="200", y2="200", stroke="blue", stroke_width="4")
-
-    svg._render_svg(empty_doc)
-    render = yattag.indent(empty_doc.getvalue())
-
-    gotten_soup = BeautifulSoup(render, "lxml")
-    expected_soup = BeautifulSoup(EXPECTED_TEST1_INLINE_OUTPUT, "lxml")
-
-    assert gotten_soup == expected_soup
-
-
-@pytest.mark.xfail(reason="Not DONE")
-def test_1_static_version():
-
-    class SvgTest1(Svgrap):
-
-        def my_overriden_paint_method(self):
-            smth = None
-            smth.stag('rect', x="25", y="25", width="200", height="200", fill="lime", stroke_width="4", stroke="pink")
-            smth.stag('circle', cx="125", cy="125", r="75", fill="orange")
-            smth.stag('polyline', ('stroke-width', 4),
-                      points="50,150 50,200 200,200 200,100", stroke="red", fill="none")
-            smth.stag('line', x1="50", y1="50", x2="200", y2="200", stroke="blue", stroke_width="4")
-
-    svg = SvgTest1()
-
-    render = svg._render_svg()
-
-    gotten_soup = BeautifulSoup(render, "lxml")
-    expected_soup = BeautifulSoup(EXPECTED_TEST1_INLINE_OUTPUT, "lxml")
-
-    assert gotten_soup == expected_soup
-
-
-def test_classlike_svg_empty_svg(out_dir):
-
-    empty_page = yattag.SimpleDoc()
-
-    class EmptySvg(SvgClassBase):
+    doc = SvgHost()
+    with doc.svg():
         pass
-
-    svg = EmptySvg(empty_page)
-
-    svg_code = svg._create_svg_code()
-    assert svg_code == '<svg version="1.1" xmlns="http://www.w3.org/2000/svg"></svg>'
-
-    file_ = os.path.join(out_dir, 'svg_1.svg')
-    svg.save_to_file(file_)
-
-    assert os.path.isfile(file_), "File {} not created".format(file_)
-    with open(file_, 'rt') as ff:
-        content = ff.read()
-    print 'file content'
-    print content
-    assert content == """\
-<?xml version="1.0" encoding="UTF-8" ?>
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg"></svg>
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg"></svg>"""
+    assert doc.getvalue() == '<svg version="1.1" xmlns="http://www.w3.org/2000/svg"></svg>'
 
 
-def test_classlike_svg_simple_svg_1(out_dir):
-    empty_page = yattag.SimpleDoc()
+def test_svg_as_a_tag_1():
 
-    class MySvg(SvgClassBase):
-        def render_impl(self, painter):
-            painter.stag('rect', x="25", y="25", width="200", height="200", stroke="pink")
-
-    svg = MySvg(empty_page)
-
-    svg_code = svg._create_svg_code()
-    assert svg_code == """\
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-  <rect y="25" x="25" stroke="pink" height="200" width="200" />
-</svg>"""
-
-    file_ = os.path.join(out_dir, 'svg_1.svg')
-    svg.save_to_file(file_)
-
-    assert os.path.isfile(file_), "File {} not created".format(file_)
-    with open(file_, 'rt') as ff:
-        content = ff.read()
-    print 'file content'
-    print content
-    assert content == """\
-<?xml version="1.0" encoding="UTF-8" ?>
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-  <rect y="25" x="25" stroke="pink" height="200" width="200" />
-</svg>
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-  <rect y="25" x="25" stroke="pink" height="200" width="200" />
-</svg>"""
+    doc = SvgHost()
+    with doc.svg():
+        painter_def_test1(doc)
+    compare_htmls(yattag.indent(doc.getvalue()), EXPECTED_TEST1_OUTPUT)
 
