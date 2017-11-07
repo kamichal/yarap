0e1de1d05516ad7361ba82aba282d65eaf2117dd
kamichal <bucowski@gmail.com>
2017-11-08 00:59:18 +0100

    added tox and setup

---
+++ setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+long_description = open('README.rst').read()
+
+setup(
+    name = 'yawrap',
+    version = '0.1',
+    author = 'Michal Kaczmarczyk',
+    author_email = 'michal.s.kaczmarczyk@gmail.com',
+    maintainer = 'Michal Kaczmarczyk',
+    maintainer_email = 'michal.s.kaczmarczyk@gmail.com',
+    license = 'MIT license',
+    url = 'https://bitbucket.org/gandowin/yarap/',
+    description = 'yawrap: simple generator of complex html reports, yattag extension',
+    long_description = long_description,
+    packages = find_packages(),
+    requires = ['yattag'],
+    install_requires = ['yattag'],
+    keywords = 'static html generator',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Telecommunications Industry',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+        'Topic :: Database :: Front-Ends',
+        'Topic :: Documentation',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Browsers',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ]
+)
