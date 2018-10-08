d4731af4555de33f0a016944767279f92d1773a5
Michał Kaczmarczyk <michal.s.kaczmarczyk@gmail.com>
2018-10-08 23:28:02 +0200

    Bumpversion to 0.4.4.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name='yawrap',
-    version='0.4.3',
+    version='0.4.4',
     author='Michal Kaczmarczyk',
     author_email='michal.s.kaczmarczyk@gmail.com',
     maintainer='Michal Kaczmarczyk',
